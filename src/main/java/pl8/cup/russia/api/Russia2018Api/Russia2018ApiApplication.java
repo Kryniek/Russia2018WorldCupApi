392d45cc9f9a387c1e7f94bd452a524d92fb5876
Kryniek <shasu93@gmail.com>
2018-06-05 08:54:06 +0200

    initial commit

---
+++ src/main/java/pl8/cup/russia/api/Russia2018Api/Russia2018ApiApplication.java
@@ -0,0 +1,12 @@
+package pl8.cup.russia.api.Russia2018Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Russia2018ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Russia2018ApiApplication.class, args);
+	}
+}
