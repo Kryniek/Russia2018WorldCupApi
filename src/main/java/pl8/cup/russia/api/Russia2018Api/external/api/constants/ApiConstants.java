eb22b34173d00e24f31e1cb5b168537263c81f82
Bartek <bartosz.skorka93@gmail.com>
2018-06-06 00:51:11 +0200

    added encryption to our properties to keep mongo password and api key in encrypted variables - and for now - till we configure starting app from ide - secret key is still in properties.

---
+++ src/main/java/pl8/cup/russia/api/Russia2018Api/external/api/constants/ApiConstants.java
@@ -0,0 +1,7 @@
+package pl8.cup.russia.api.Russia2018Api.external.api.constants;
+
+public class ApiConstants {
+
+    public static final Integer WORLD_CUP_COUNTRY_ID = 340;
+
+}
--- src/main/java/pl8/cup/russia/api/Russia2018Api/external/api/model/ApiLeague.java
+++ src/main/java/pl8/cup/russia/api/Russia2018Api/external/api/model/ApiLeague.java
@@ -1,4 +1,13 @@
 package pl8.cup.russia.api.Russia2018Api.external.api.model;
 
 public class ApiLeague {
+
+    private Integer countryId;
+
+    private String countryName;
+
+    private Integer leagueId;
+
+    private String leagueName;
+
 }
