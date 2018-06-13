365927e271e9b5ff9affcb3afaf4adb5f0374161
Kryniek <shasu93@gmail.com>
2018-06-13 13:41:44 +0200

    -go back form buttons -winner after update/insert message -points description update -some small changes

--- src/main/java/pl/cup/russia/api/Russia2018Api/controller/BetController.java
+++ src/main/java/pl/cup/russia/api/Russia2018Api/controller/BetController.java
@@ -1,20 +1,26 @@
 package pl.cup.russia.api.Russia2018Api.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import pl.cup.russia.api.Russia2018Api.definition.BetService;
-import pl.cup.russia.api.Russia2018Api.definition.LeagueService;
-import pl.cup.russia.api.Russia2018Api.dto.rest.BetValue;
-import pl.cup.russia.api.Russia2018Api.enums.StaticHtmlResource;
-import pl.cup.russia.api.Russia2018Api.model.Bet;
-
-import java.util.List;
-
 import static pl.cup.russia.api.Russia2018Api.enums.BetType.GROUP_STAGE_PROMOTION;
 import static pl.cup.russia.api.Russia2018Api.enums.BetType.WORLD_CUP_WINNER;
 import static pl.cup.russia.api.Russia2018Api.util.BetValidator.canBetWorldCupWinner;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import pl.cup.russia.api.Russia2018Api.definition.BetService;
+import pl.cup.russia.api.Russia2018Api.definition.LeagueService;
+import pl.cup.russia.api.Russia2018Api.dto.rest.BetValue;
+import pl.cup.russia.api.Russia2018Api.enums.DBOperation;
+import pl.cup.russia.api.Russia2018Api.enums.StaticHtmlResource;
+import pl.cup.russia.api.Russia2018Api.model.Bet;
 
 @Controller
 @RequestMapping("/bets")
@@ -30,22 +36,22 @@ public class BetController {
 	public String betWorldCupWinner(@PathVariable String winnerTeamName, RedirectAttributes redirectAttributes) {
 		service.createWorldCupWinnerBet(winnerTeamName);
 
-		return getWorldCupWinnerRedirectWithAttributes(redirectAttributes);
+		return getWorldCupWinnerRedirectWithAttributes(redirectAttributes, DBOperation.INSERTED);
 	}
 
 	@PutMapping("/winner/{winnerTeamName}")
 	public String updateBetWorldCupWinner(@PathVariable String winnerTeamName, RedirectAttributes redirectAttributes) {
 		service.updateBetByType(WORLD_CUP_WINNER, new BetValue(winnerTeamName));
 
-		return getWorldCupWinnerRedirectWithAttributes(redirectAttributes);
+		return getWorldCupWinnerRedirectWithAttributes(redirectAttributes, DBOperation.UPDATED);
 	}
 
-    @PostMapping("/group")
-    public List<Bet> betGroupPromotion(@ModelAttribute("betValues") List<BetValue> betValues) {
-        return service.createGroupPromotionBets(betValues);
-    }
+	@PostMapping("/group")
+	public List<Bet> betGroupPromotion(@ModelAttribute("betValues") List<BetValue> betValues) {
+		return service.createGroupPromotionBets(betValues);
+	}
 
-    @PutMapping("/group")
+	@PutMapping("/group")
 	public Integer updateBetGroupPromotion(@ModelAttribute("betValues") List<BetValue> betValues) {
 		return service.updateBetsByType(GROUP_STAGE_PROMOTION, betValues);
 	}
@@ -55,10 +61,12 @@ public class BetController {
 		return service.createMatchScoreBet(betValue);
 	}
 
-	private String getWorldCupWinnerRedirectWithAttributes(RedirectAttributes redirectAttributes) {
+	private String getWorldCupWinnerRedirectWithAttributes(RedirectAttributes redirectAttributes,
+			DBOperation operation) {
 		redirectAttributes.addFlashAttribute("teams", leagueService.selectAllTeams());
 		redirectAttributes.addFlashAttribute("userBet", service.selectUserBetByType(WORLD_CUP_WINNER));
 		redirectAttributes.addFlashAttribute("canYouBet", canBetWorldCupWinner());
+		redirectAttributes.addFlashAttribute("operation", operation.name());
 
 		return StaticHtmlResource.WORLD_CUP_WINNER.getKebabCasedRedirectValue();
 	}
---
+++ src/main/java/pl/cup/russia/api/Russia2018Api/enums/DBOperation.java
@@ -0,0 +1,5 @@
+package pl.cup.russia.api.Russia2018Api.enums;
+
+public enum DBOperation {
+	INSERTED, UPDATED;
+}
